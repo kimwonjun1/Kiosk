ae60ca6bb538627f28bac07145ff848f55c4676c
kimwonjun1 <monkeydmw@naver.com>
2025-04-24 12:04:35 +0900

    MenuItem class create

--- src/com/example/kiosk/Main.java
+++ src/com/example/kiosk/Main.java
@@ -1,4 +1,14 @@
-package com.example.kiosk;
+package com.example.kiosklv1;
+
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("[ SHAKESHACK MENU ]");
+        System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+        System.out.println("");
+        sc.nextInt();
+    }
 }
