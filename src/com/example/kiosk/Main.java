12b78dd88194b1b5fce92e9e4450d01d256c7d03
kimwonjun1 <monkeydmw@naver.com>
2025-04-28 17:31:07 +0900

    Main class Comment processing

--- src/com/example/kiosk/Main.java
+++ src/com/example/kiosk/Main.java
@@ -2,13 +2,12 @@ package com.example.kiosk;
 
 public class Main {
     public static void main(String[] args) {
-        // Menu 객체 생성을 통해 이름 설정
-//        Menu menu = new Menu("Burgers");
-        // Menu 클래스 내 있는 List<MenuItem> 에 MenuItem 객체 생성하면서 삽입
+        // Menu 객체 생성을 통해 이름 설정 -> Kiosk 클래스에서 구현
+        // Menu 클래스 내 있는 List<MenuItem> 에 MenuItem 객체 생성하면서 삽입 -> Menu 클래스에서 구현
 
         // Kiosk 객체 생성
         Kiosk kiosk = new Kiosk();
-        // Kiosk 내 시작하는 함수 호출
+        // Kiosk 객체의 start 메서드 호출
         kiosk.start();
     }
 }
