4e1aa620287ae0790270ae004a63ae3f8b3f94da
kimwonjun1 <monkeydmw@naver.com>
2025-04-28 16:20:42 +0900

    MenuItem class Comment processing

--- src/com/example/kiosk/MenuItem.java
+++ src/com/example/kiosk/MenuItem.java
@@ -1,14 +1,13 @@
 package com.example.kiosk;
 
 public class MenuItem {
-    // 속성. 캡슐화를 위해 private로 설정
+    // 속성. 캡슐화를 위해 private로 설정. 외부에서 MenuItem의 정보(이름, 가격, 설명)을 변경 불가
     private String name; // 이름
     private double price; // 가격
     private String description; // 설명
 
-    // setter
-
-    // getter
+    // 기능
+    // getter - MenuItem의 정보(이름, 가격, 설명)을 외부에서 가져갈때 사용
     public String getName() {
         return name;
     }
@@ -22,6 +21,7 @@ public class MenuItem {
     }
 
     // 생성자
+    // MenuItem 객체 생성 시 이름, 가격, 설명을 설정
     public MenuItem(String name, double price, String description) {
         this.name = name;
         this.price = price;
